c5c6f3f340176a6d8f3b00570c2b4a0ef62e8368
thetinkeringee <>
2024-12-30 15:43:02 -0500

    Moved license into license.h

---
+++ src/license.h
@@ -0,0 +1,46 @@
+//  Wayland from Scratch
+//  Copyright (C) 2024 Ezekiel Holliday
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef LICENSE_H_20241230
+#define LICENSE_H_20241230
+// TODO: Fix what is displayed whith the --L and the rest of the license
+//
+static const char *license =
+    "  Wayland from Scratch \n"
+    "  Copyright (C) 2024 Ezekiel Holliday \n"
+    "\n"
+    "  This program is free software: you can redistribute it and/or modify\n"
+    "  it under the terms of the GNU General Public License as published by\n"
+    "  the Free Software Foundation, either version 3 of the License, or \n"
+    "  (at your option) any later version.\n"
+    "\n"
+    "  This program is distributed in the hope that it will be useful,\n"
+    "  but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+    "  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+    "  GNU General Public License for more details.\n"
+    "\n"
+    "  You should have received a copy of the GNU General Public License\n"
+    "  along with this program.  If not, see "
+    "<https://www.gnu.org/licenses/>.\n";
+
+static const char *const license_banner =
+    "Wayland from Scratch  Copyright (C) 2024 Ezekiel Holliday\n"
+    "This program comes with ABSOLUTELY NO WARRANTY; for details run the "
+    "program wiht --L.\n"
+    "This is free software, and you are welcome to redistribute it\n"
+    "under certain conditions; run the program with --L for details.\n";
+
+#endif
